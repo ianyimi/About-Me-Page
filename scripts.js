63fe715dff39665f4eef95a1b8f50490e02f1b8b
Isaiah <anyiminene@gmail.com>
2018-11-19 20:52:23 -0800

    bare js and more html content and css

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
-//function setup() {
-  createCanvas(700,700);
-};
-//function draw() {
-  ellipse(mouseX, mouseY,100,100)
-};
+//https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.travelandleisure.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F1600x1000%2Fpublic%2F1515188208%2Fskyline-sacramento-california-LADYBIRD0118.jpg%3Fitok%3DNer4TI-0&w=1000&c=sc&poi=face&q=70
+//http://www.ishtar.it/wp/wp-content/uploads/2017/10/blueSpiritStudio_04.jpg studio
+document.querySelector("#startGame").addEventListener("click", function(event) {
+  this.style.display = "none";
+})
